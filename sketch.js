95b0a6412725fe9c22c7748ba01e18eecb07229c
Xavier Bacalla <49704299+xavierbacalla@users.noreply.github.com>
2019-04-28 19:24:08 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
-var s = 50; // image size
+var s = 50; // cursor image size
 var offset = 30;
+
 let img;
 let canvas;
 let extraCanvas;
@@ -14,23 +15,16 @@ function preload() {
 function setup() {
   canvas = createCanvas(300, 200);
   canvas.parent('sketch-holder');
-  pixelDensity(1);
 
-
-  extraCanvas = createGraphics(width - offset, height - offset);
+  extraCanvas = createGraphics((width - offset) * pixelDensity(), (height - offset) * pixelDensity());
   extraCanvas.parent('sketch-holder');
 
-  //extraCanvas.background(255, 80, 25);
-  //extraCanvas.clear();
-
-  //background(0, 80, 25);
   resetSketch();
 
   button = createButton('reset');
   button.parent('sketch-holder');
   button.mousePressed(resetSketch);
   button.position(button.center('horizontal'), height + 25);
-  //cursor(CROSS);
 
   noCursor();
 }
@@ -47,14 +41,12 @@ function draw() {
 
   // no trails
   background(255);
-  image(extraCanvas, offset / 2, offset / 2);
+  image(extraCanvas, offset / 2, offset / 2, width - offset, height - offset);
   image(img, mouseX - s / 2, mouseY - s / 2, s, s);
-
 }
 
 
 function resetSketch() {
-  //clear();
   background(255);
   extraCanvas.background(192);
 }
